b332c5b2158759dc2750457c15438d12b539e620
박종석 <jsp1256@gmail.com>
2020-02-24 20:05:48 +0900

    Initial Commit

---
+++ Tap WaterTests/Tap_WaterTests.swift
@@ -0,0 +1,34 @@
+//
+//  Tap_WaterTests.swift
+//  Tap WaterTests
+//
+//  Created by 박종석 on 2020/02/24.
+//  Copyright © 2020 박종석. All rights reserved.
+//
+
+import XCTest
+@testable import Tap_Water
+
+class Tap_WaterTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
